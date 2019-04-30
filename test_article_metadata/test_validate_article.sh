66db01ba21e225dafcb9e005f2f62a4c5050a43e
William McLendon <wcmclen@sandia.gov>
2019-04-30 12:34:51 -0600

    rough in travisCI for test scripts

--- test_article_metadata/test_validate_article.sh
+++ test_article_metadata/test_validate_article.sh
@@ -1,32 +1,68 @@
 #!/usr/bin/env bash
 
-debug_flag=""
+debug_flag="-V"
 
 metadata_config=config-metadata.txt
 
 color_tty=""
 color_tty="--color=tty"
 
+test_dir=test_data
+
+test_articles=(
+    FAIL    ${test_dir:?}/test_001_fail.md
+    PASS    ${test_dir:?}/test_002_pass.md
+    PASS    ${test_dir:?}/test_003_pass.md
+    PASS    ${test_dir:?}/test_004_pass.md
+    PASS    ${test_dir:?}/test_005_pass.md
+    PASS    ${test_dir:?}/test_006_pass.md
+    PASS    ${test_dir:?}/test_007_pass.md
+    PASS    ${test_dir:?}/test_008_pass.md
+    PASS    ${test_dir:?}/test_009_pass.md
+    FAIL    ${test_dir:?}/test_010_fail.md
+    FAIL    ${test_dir:?}/test_011_fail.md
+    FAIL    ${test_dir:?}/test_012_fail.md
+    FAIL    ${test_dir:?}/test_pr_0344.md
+)
+
+num_tests=0
+num_success=0
+num_failure=0
+
 echo ""
-python validate_article.py ${debug_flag} -f test_data/test_file_001.md -s ${metadata_config} ${color_tty}
+for ((i=0; i<${#test_articles[@]}; i+=2)); do
+    expected_result=${test_articles[i]}
+    filename=${test_articles[i+1]}
+    echo -e "Testing: ${expected_result}\t${filename}"
+    cmd="python validate_article.py ${debug_flag} -f ${filename} -s ${metadata_config} ${color_tty}"
+    let "num_tests+=1"
+    ${cmd:?} >& /dev/null
+    err=$?
+    if [[ "${expected_result}" == "PASS" ]] && [[ "${err:?}" != "0" ]]; then
+        echo -e "${cmd:?}"
+        echo -e "FAILURE"
+        let "num_failure+=1"
+    elif [[ "${expected_result}" == "FAIL" ]] && [[ "${err:?}" == "0" ]]; then
+        echo -e "${cmd:?}"
+        echo -e "FAILURE"
+        let "num_failure+=1"
+    else
+        echo -e "SUCCESS"
+        let "num_success+=1"
+    fi
+    echo -e ""
+done
 
 
-echo "================================================================================"
-echo ""
-python validate_article.py ${debug_flag} -f test_data/test_file_002.md -s ${metadata_config} ${color_tty}
+echo -e "==============================="
+echo -e "  Num Tests  : ${num_tests:?}"
+echo -e "  Num SUCCESS: ${num_success:?}"
+echo -e "  Num FAILURE: ${num_failure:?}"
+echo -e "==============================="
 
+if [[ ${num_failure:?} != 0 ]]; then
+    echo -e "Tests failed."
+    exit 1
+fi
 
-echo "================================================================================"
-echo ""
-python validate_article.py ${debug_flag} -f test_data/test_file_003.md -s ${metadata_config} ${color_tty}
-
-
-echo "================================================================================"
-echo ""
-python validate_article.py ${debug_flag} -f test_data/test_file_004.md -s ${metadata_config} ${color_tty}
-
-
-echo "================================================================================"
-echo ""
-python validate_article.py ${debug_flag} -f test_data/test_file_005.md -s ${metadata_config} ${color_tty}
 
